5daf9aa4b4414e72aa6ba8bac01a5a8c507c63e8
99b2965b6a <99b2965b6a@github.com>
2026-02-24 12:45:13 +0200

    initial commit: setup project structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="dashboard-1771929889-2",
+    version="1.0.0",
+    description="Professional Python library",
+    author="99b2965b6a",
+    packages=find_packages(),
+    python_requires=">=3.8"
+)
